024d2c16d0c398b3f5d5ff0f36ce7ec1d526c538
omanpeko <79346126+omanpeko@users.noreply.github.com>
2026-02-21 20:18:15 +0900

    Add files via upload

---
+++ SF6/firebase-config.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAjoNM6nvGusQn4Jeth2tjUWITu6tOSXDE",
+
+  authDomain: "haisin-da6f0.firebaseapp.com",
+
+  projectId: "haisin-da6f0",
+
+  storageBucket: "haisin-da6f0.firebasestorage.app",
+
+  messagingSenderId: "68927516571",
+
+  appId: "1:68927516571:web:cefbd724ad5691fcf33b3e",
+
+  measurementId: "G-F0P5FD1E7J"
+
+};
+
+
+firebase.initializeApp(firebaseConfig);
